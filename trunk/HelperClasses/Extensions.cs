52d9c6caff97c131738b7c44fb48e3a927a58fd3
eulalie367 <eulalie367@69dc8a64-c186-1442-b51f-188cc4f97be4>
2010-08-10 16:04:53 +0000

    git-svn-id: https://littleapps.googlecode.com/svn@13 69dc8a64-c186-1442-b51f-188cc4f97be4

---
+++ trunk/HelperClasses/Extensions.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Collections;
+using System.Web.UI.HtmlControls;
+
+namespace SolutionMatchTool.Data
+{
+    public static class Extensions
+    {
+        public static int? ToInt(this HttpRequest req, string key)
+        {
+            int? retVal = null;
+            try
+            {
+                int tmp = -1;
+                if (req[key] != null && !string.IsNullOrEmpty(req[key]))
+                    if (int.TryParse(req[key], out tmp))
+                        retVal = tmp;
+            }
+            catch
+            { }
+            return retVal;
+        }
+        public static int? ToInt(this string str)
+        {
+            int? retVal = null;
+            try
+            {
+                int tmp = -1;
+                if (!string.IsNullOrEmpty(str))
+                    if (int.TryParse(str, out tmp))
+                        retVal = tmp;
+            }
+            catch
+            { }
+            return retVal;
+        }
+        public static DateTime? ToDateTime(this string str)
+        {
+            DateTime? retVal = null;
+            try
+            {
+                DateTime tmp;
+                if (!string.IsNullOrEmpty(str))
+                    if (DateTime.TryParse(str, out tmp))
+                        retVal = tmp;
+            }
+            catch
+            { }
+            return retVal;
+        }
+        public static Boolean? ToBool(this string str)
+        {
+            if (!string.IsNullOrEmpty(str))
+            {
+                switch (str.ToLower())
+                {
+                    case "yes":
+                        return true;
+                        break;
+                    case "no":
+                        return false;
+                        break;
+                    case "true":
+                        return true;
+                        break;
+                    case "false":
+                        return false;
+                        break;
+                    case "1":
+                        return true;
+                        break;
+                    case "0":
+                        return true;
+                        break;
+                    default:
+                        return null;
+                        break;
+                }
+            }
+            else
+                return null;
+        }
+        public static void FillSelect<t>(this DropDownList select, t source, string nameCol, string valCol, string index0) where t : IEnumerable
+        {
+            if (!string.IsNullOrEmpty(index0))
+            {
+                select.Items.Add(new ListItem(index0, ""));
+                select.AppendDataBoundItems = true;
+            }
+            select.DataSource = source;
+            select.DataTextField = nameCol;
+            select.DataValueField = valCol;
+            select.DataBind();
+        }
+        public static void FillSelect<t>(this HtmlSelect select, t source, string nameCol, string valCol) where t : IEnumerable
+        {
+            select.DataSource = source;
+            select.DataTextField = nameCol;
+            select.DataValueField = valCol;
+            select.DataBind();
+        }
+    }
+}
---
+++ trunk/HelperClasses/SqlHelper.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Reflection;
+using SolutionMatchTool.DAL;
+
+namespace SolutionMatchTool.Data
+{
+    public class SqlHelper
+    {
+        private static string connString;
+        public static string ConnString
+        {
+            set
+            {
+                connString = value;
+            }
+            get
+            {
+                if (string.IsNullOrEmpty(connString))
+                    connString = ConfigurationManager.ConnectionStrings["SAP_BOBJ"].ConnectionString;
+                return connString;
+            }
+        }
+        public static object ExecuteScalar(string sql)
+        {
+            object retVal = null;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnString))
+                {
+                    using (SqlCommand com = new SqlCommand(sql, conn))
+                    {
+                        conn.Open();
+
+                        retVal = com.ExecuteScalar();
+
+                        conn.Close();
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                //TODO:add error handling
+            }
+
+            return retVal;
+        }
+        public static int ExecuteNonQuery(string sql)
+        {
+            return ExecuteNonQuery(sql, null, CommandType.Text);
+        }
+        public static int ExecuteNonQuery(string sql, SqlParameter[] Params, CommandType commandType)
+        {
+            int retVal = 0;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnString))
+                {
+                    using (SqlCommand com = new SqlCommand(sql, conn))
+                    {
+                        com.CommandType = commandType;
+                        if (Params != null && Params.Length > 0)
+                            com.Parameters.AddRange(Params);
+
+                        conn.Open();
+
+                        retVal = com.ExecuteNonQuery();
+
+                        conn.Close();
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                //TODO:add error handling
+            }
+
+            return retVal;
+        }
+        /// <summary>
+        /// This will give all results the correct type
+        /// You will need to make sure that the properties in the class you pass are named the same as the dbcolumn
+        /// I would suggest using a tool like sqlmetal to generate your classes.
+        /// </summary>
+        /// <typeparam name="t"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="Params"></param>
+        /// <param name="commandType"></param>
+        /// <returns></returns>
+        public static List<t> FillEntities<t>(string sql) where t : new()
+        {
+            return FillEntities<t>(sql, null, CommandType.Text);
+        }
+        /// <summary>
+        /// This will give all results the correct type
+        /// You will need to make sure that the properties in the class you pass are named the same as the dbcolumn
+        /// I would suggest using a tool like sqlmetal to generate your classes.
+        /// </summary>
+        /// <typeparam name="t"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="Params"></param>
+        /// <param name="commandType"></param>
+        /// <returns></returns>
+        public static List<t> FillEntities<t>(string sql, SqlParameter[] Params, CommandType commandType) where t : new()
+        {
+            //use this to make a local copy of the db, so you can at least see the schema.
+            //using (DontUseThisUnlessYouAreStupid d = new DontUseThisUnlessYouAreStupid(""))
+            //{
+            //    d.CreateDatabase();
+            //}
+
+            List<t> retVal = new List<t>();
+            t tmp = default(t);
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnString))
+                {
+                    using (SqlCommand com = new SqlCommand(sql, conn))
+                    {
+                        com.CommandType = commandType;
+                        if (Params != null && Params.Length > 0)
+                            com.Parameters.AddRange(Params);
+
+                        conn.Open();
+
+                        using (SqlDataReader reader = com.ExecuteReader())
+                        {
+                            string colName = "";
+                            object value = null;
+                            Type type = typeof(t);
+                            PropertyInfo prop = null;
+                            while (reader.Read())
+                            {
+                                tmp = new t();
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    colName = reader.GetName(i);
+                                    value = reader[i];
+
+                                    if (string.IsNullOrEmpty(colName))
+                                        prop = type.GetProperties()[i];
+                                    else
+                                        prop = type.GetProperty(colName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+
+                                    SetPropetyValue(tmp, value, prop);
+                                }
+                                retVal.Add(tmp);
+                            }
+                            reader.Close();
+                        }
+                        conn.Close();
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                //TODO:add error handling
+            }
+
+
+            return retVal;
+        }
+        /// <summary>
+        /// This will give all results the correct type
+        /// You will need to make sure that the properties in the class you pass are named the same as the dbcolumn
+        /// I would suggest using a tool like sqlmetal to generate your classes.
+        /// </summary>
+        /// <typeparam name="t"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="Params"></param>
+        /// <param name="commandType"></param>
+        /// <returns></returns>
+        public static t FillEntity<t>(string sql) where t : new()
+        {
+            return FillEntity<t>(sql, null, CommandType.Text);
+        }
+        /// <summary>
+        /// This will give all results the correct type
+        /// You will need to make sure that the properties in the class you pass are named the same as the dbcolumn
+        /// I would suggest using a tool like sqlmetal to generate your classes.
+        /// </summary>
+        /// <typeparam name="t"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="Params"></param>
+        /// <param name="commandType"></param>
+        /// <returns></returns>
+        public static t FillEntity<t>(string sql, SqlParameter[] Params, CommandType commandType) where t : new()
+        {
+            t tmp = default(t);
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnString))
+                {
+                    using (SqlCommand com = new SqlCommand(sql, conn))
+                    {
+                        com.CommandType = commandType;
+                        if (Params != null && Params.Length > 0)
+                            com.Parameters.AddRange(Params);
+
+                        conn.Open();
+
+                        using (SqlDataReader reader = com.ExecuteReader())
+                        {
+                            string colName = "";
+                            object value = null;
+                            Type type = typeof(t);
+                            PropertyInfo prop = null;
+
+                            if (reader.Read())
+                            {
+                                tmp = new t();
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    colName = reader.GetName(i);
+                                    value = reader[i];
+
+                                    prop = type.GetProperty(colName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                                    SetPropetyValue(tmp, value, prop);
+                                }
+                            }
+                            reader.Close();
+                        }
+                        conn.Close();
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                //TODO:add error handling
+            }
+
+
+            return tmp;
+        }
+
+        private static void SetPropetyValue(object tmp, object value, PropertyInfo prop)
+        {
+            if (!(value is DBNull) && prop != null)
+            {
+                if (prop.PropertyType != typeof(System.Xml.Linq.XElement))//this really comes back as a string from the db.
+                    prop.SetValue(tmp, value, null);
+                else
+                {
+                    if (value != null)
+                    {
+                        string tmpVal = value.ToString();
+                        if (!string.IsNullOrEmpty(tmpVal))
+                        {
+                            try
+                            {
+                                System.Xml.Linq.XElement elem = System.Xml.Linq.XElement.Parse(tmpVal);
+                                prop.SetValue(tmp, elem, null);
+                            }
+                            catch 
+                            {
+                                try//make sure there is a root node
+                                {
+                                    System.Xml.Linq.XElement elem = System.Xml.Linq.XElement.Parse(string.Format("<rootasdf>{0}</rootasdf>", tmpVal));
+                                    prop.SetValue(tmp, elem, null);
+                                }
+                                catch
+                                { }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
