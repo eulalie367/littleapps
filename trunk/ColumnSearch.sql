dc1a3a58047cb5f630383528d6a6ccf690d73bfd
eulalie367 <eulalie367@69dc8a64-c186-1442-b51f-188cc4f97be4>
2010-12-20 07:47:49 +0000

    git-svn-id: https://littleapps.googlecode.com/svn@35 69dc8a64-c186-1442-b51f-188cc4f97be4

---
+++ trunk/ColumnSearch.sql
@@ -0,0 +1,34 @@
+SELECT 
+o.name tableName,
+c.name columnName,
+c.is_nullable,
+t.name as type,
+c.max_length,
+dc.definition defaulValue,
+c.name + ' '
+	+ t.name 
+	+ case t.name 
+		WHEN 'varchar'then ' (' + CONVERT(varchar,c.max_length) + ')' 
+		WHEN 'char'then ' (' + CONVERT(varchar,c.max_length) + ')'
+		else ''
+	  end
+	+ case 
+		WHEN c.is_nullable = 0 and not dc.object_id is null then ' NOT NULL DEFAULT ' + REPLACE(REPLACE(CONVERT(varchar,dc.definition), '(',''),')','')
+		WHEN c.is_nullable = 0 then ' NOT NULL'
+		else ''
+	  end
+	+ ','
+as createStatement,
+c.name + ',' columnList,
+c.name + ' = st.' + c.name + ',' LinqSelect
+
+FROM sys.objects o
+inner join sys.columns c on c.object_id = o.object_id 
+	and o.name like '%historical%'
+	and c.name like '%%'
+inner join sys.types t on t.user_type_id = c.user_type_id 
+left join sys.default_constraints dc on dc.object_id = c.default_object_id
+	and c.is_nullable = 0
+
+
+
