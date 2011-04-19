94749252290323a07c657a2c47f905f36ac83f13
eulalie367 <eulalie367@69dc8a64-c186-1442-b51f-188cc4f97be4>
2011-04-19 02:09:05 +0000

    git-svn-id: https://littleapps.googlecode.com/svn@112 69dc8a64-c186-1442-b51f-188cc4f97be4

--- trunk/Small Business Website Solutions/Corporate Site/Src/masterpages/CorporateSite_Interior.master.cs
+++ trunk/Small Business Website Solutions/Corporate Site/Src/masterpages/CorporateSite_Interior.master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using umbraco.NodeFactory;
 
 namespace SBWS.masterpages
 {
@@ -11,7 +12,17 @@ namespace SBWS.masterpages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Node current = Node.GetCurrent();
 
+            while (Menu1.ParentNodeID < 1 && current != null && current.Level > 0)
+            {
+                Property p = current.GetProperty("parentNodeID") as Property;
+                if (p != null && !string.IsNullOrEmpty(p.Value))
+                {
+                    Menu1.ParentNodeID = p.Value.ToInt() ?? 0;
+                }
+                current = current.Parent as Node;
+            }
         }
     }
 }
