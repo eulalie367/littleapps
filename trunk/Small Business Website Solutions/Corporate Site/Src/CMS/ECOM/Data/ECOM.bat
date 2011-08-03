94129f0d4757e5e8900279a0ae39193efefd9f5c
eulalie367 <eulalie367@69dc8a64-c186-1442-b51f-188cc4f97be4>
2011-08-03 19:38:43 +0000

    git-svn-id: https://littleapps.googlecode.com/svn@140 69dc8a64-c186-1442-b51f-188cc4f97be4

--- trunk/Small Business Website Solutions/Corporate Site/Src/CMS/ECOM/Data/ECOM.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Microsoft SDKs\Windows\v7.0A\bin\sqlmetal.exe" /conn:"Integrated Security=true;Initial Catalog=ECOM;Server=PADDY-674ABAE17" /namespace:SBWS.ECOM.Data /code:ECOM.cs /language:csharp /pluralize /views /functions /sprocs /context:UseSQLHelperAndNotThisContext
-pause
