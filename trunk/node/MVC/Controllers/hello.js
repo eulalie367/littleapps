4ff02c4470b29873540e331815eaa98c62ba400e
eulalie367 <eulalie367@69dc8a64-c186-1442-b51f-188cc4f97be4>
2012-08-17 06:51:03 +0000

    Views
    git-svn-id: https://littleapps.googlecode.com/svn@153 69dc8a64-c186-1442-b51f-188cc4f97be4

---
+++ trunk/node/MVC/Controllers/hello.js
@@ -0,0 +1,21 @@
+exports.index = function(request, response, filename, callback)
+{
+	
+	var model = {
+	  title: "Joe",
+	  calc: function() {
+		 return 2 + 4;
+	  }
+	};
+	callback(model, filename, response);
+}
+
+exports.test = function()
+{
+	return "/Hello/Test";
+}
+		exports.test.test = function()
+	{
+		return "/Hello/Test/Test";
+	}
+
